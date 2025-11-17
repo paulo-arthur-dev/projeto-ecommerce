d785cca964de4a6a61eb97b13b455c648036c921
Paulo Arthur <pauloarthurm13@gmail.com>
2025-11-16 23:44:58 -0300

    imgs produtos e  alters finais

--- js/script.js
+++ js/script.js
@@ -13,10 +13,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //Produtos
     const produtos = [
-        { id: 1, nome: 'Smartphone', categoria: 'eletronicos', preco: 1500, img: 'imgs/shop-placeholder.png' },
-        { id: 2, nome: 'Notebook', categoria: 'eletronicos', preco: 3000, img: 'imgs/shop-placeholder.png' },
-        { id: 3, nome: 'Livro', categoria: 'livros', preco: 100, img: 'imgs/shop-placeholder.png' },
-        { id: 4, nome: 'Camiseta', categoria: 'vestuario', preco: 50, img: 'imgs/shop-placeholder.png' }
+        
+        //Eletrônicos
+        { id: 1, nome: 'Iphone 17 Pro', categoria: 'eletronicos', preco: 12699.99, img: 'imgs/eletronicos/iphone-17-pro.png' },
+        { id: 2, nome: 'Notebook Samsung', categoria: 'eletronicos', preco: 2099.99, img: 'imgs/eletronicos/notebook-samsung.png' },
+        { id: 2, nome: 'Samsung Galaxy S25', categoria: 'eletronicos', preco: 3949.99, img: 'imgs/eletronicos/Samsung-Galaxy-S25-Ultra-Flagship-Smartphone-Samsung.png' },
+        { id: 2, nome: 'Notebook Dell', categoria: 'eletronicos', preco: 2699, img: 'imgs/eletronicos/notebook-dell-inspiron.png' },
+        { id: 2, nome: 'Moto G75', categoria: 'eletronicos', preco: 1200.50, img: 'imgs/eletronicos/Moto-G75-Azul-700x700.png' },
+        { id: 2, nome: 'Notebook Asus', categoria: 'eletronicos', preco: 2979, img: 'imgs/eletronicos/notebook-asus.png' },
+        { id: 2, nome: 'Smart TV LG', categoria: 'eletronicos', preco: 1999, img: 'imgs/eletronicos/smart-tv-lg.png' },
+        { id: 2, nome: 'Xiaomi Redmi Note 14', categoria: 'eletronicos', preco: 2000, img: 'imgs/eletronicos/xiaomi-redmi-note-14.png' },
+        { id: 2, nome: 'Smart TV Samsung', categoria: 'eletronicos', preco: 3000, img: 'imgs/eletronicos/smart-tv-samsung.png' },
+
+        //Móveis
+        { id: 2, nome: 'Armário', categoria: 'moveis', preco: 349.99, img: 'imgs/moveis/armario.png' },
+        { id: 2, nome: 'Guarda-roupa', categoria: 'moveis', preco: 854.99, img: 'imgs/moveis/guarda-roupas.png' },
+        { id: 2, nome: 'Mesa Jantar', categoria: 'moveis', preco: 590.99, img: 'imgs/moveis/mesa-de-jantar.png' },
+        { id: 2, nome: 'Sofá Retrátil', categoria: 'moveis', preco: 829.99, img: 'imgs/moveis/sofa.png' },
+
+        //Eletrodomésticos
+        { id: 2, nome: 'Fogão Electrolux', categoria: 'eletrodomesticos', preco: 1549, img: 'imgs/eletrodomesticos/fogao-electrolux.png' },
+        { id: 2, nome: 'Geladeira Electrolux', categoria: 'eletrodomesticos', preco: 3099, img: 'imgs/eletrodomesticos/geladeira-electrolux.png' },
+        { id: 2, nome: 'Lavadora Brastemp', categoria: 'eletrodomesticos', preco: 1899.99, img: 'imgs/eletrodomesticos/lavadora-brastemp.png' },
+        { id: 2, nome: 'Lavadora Electrolux', categoria: 'eletrodomesticos', preco: 1449, img: 'imgs/eletrodomesticos/lavadora-electrolux.png' },
+        { id: 2, nome: 'Microondas Electrolux', categoria: 'eletrodomesticos', preco: 720, img: 'imgs/eletrodomesticos/microondas-electrolux.png' },
+        
+        //Vestuário
+        { id: 2, nome: 'Camisa Polo', categoria: 'vestuario', preco: 79.90, img: 'imgs/vestuario/camisa-polo.png' },
+        { id: 2, nome: 'Camisa Social', categoria: 'vestuario', preco: 259.90, img: 'imgs/vestuario/camisa-social.png' },
+        { id: 2, nome: 'Óculos Ray-Ban', categoria: 'vestuario', preco: 580.99, img: 'imgs/vestuario/oculos-ray-ban.png' },
+        { id: 2, nome: 'Tênis Air Jordan', categoria: 'vestuario', preco: 769.99, img: 'imgs/vestuario/tenis-nike-air-jordan.png' },
+        { id: 2, nome: 'Tênis Nike', categoria: 'vestuario', preco: 529.90, img: 'imgs/vestuario/tenis-nike.png' }
     ]
 
     //Filtrar produtos e exibir na lista
@@ -42,8 +69,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 precoProduto.textContent = `${produto.preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`
 
                 //Imagem do produto
-                const imgProduto = document.createElement('img')
-                imgProduto.src = produto.img
+                const imgProduto = document.createElement('div')
+                imgProduto.className = 'img-produto'
+                imgProduto.style.backgroundImage = `url(${produto.img})`
 
                 //Botão para adicionar ao carrinho
                 const botaoAdicionar = document.createElement('button')
